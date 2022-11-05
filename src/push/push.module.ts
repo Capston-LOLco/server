05a3f7f55c87ac861b89354ae422e078f1bc45bb
seongwon-gitcodes <ysw991106@gmail.com>
2022-11-05 17:25:27 +0900

    user module inport

--- src/push/push.module.ts
+++ src/push/push.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PushController } from './push.controller';
 import { CamService } from 'src/cam/cam.service';
 import { PushService } from './push.service';
+import { UserService } from 'src/user/user.service';
 
 @Module({
-  imports: [CamService],
+  imports: [CamService, UserService],
   controllers: [PushController],
   providers: [PushService],
 })
