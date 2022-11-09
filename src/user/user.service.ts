6583440847a803d6a819c14c02b588950ba3e11f
seongwon-gitcodes <ysw991106@gmail.com>
2022-11-10 02:59:33 +0900

    유저 서비스 불필요 코드 삭제

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { throws } from 'assert';
 import { EncryptUtil } from 'src/utiles/encrypt.util';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+// import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -60,13 +59,13 @@ export class UserService {
     }
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+  // update(id: number, updateUserDto: UpdateUserDto) {
+  //   return `This action updates a #${id} user`;
+  // }
 
-  async remove(user_id: string) {
-    // remove user
-  }
+  // async remove(user_id: string) {
+  //   // remove user
+  // }
 
   async getPushTokenByUserId(user_id: string) {
     return (await this.findOne(user_id)).push_token;
