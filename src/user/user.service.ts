6d4a9ee940cf183139c700be5eec5c7f04fdc738
seongwon-gitcodes <ysw991106@gmail.com>
2022-12-08 02:15:14 +0900

    회원가입 중복 방지

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -19,6 +19,12 @@ export class UserService {
     console.log('start - UserService.create');
     const { user_id, user_pw, user_name } = createUserDto;
 
+    const validateUser = this.findOne(user_id);
+
+    if (validateUser) {
+      throw new Error('이미 등록된 회원이 존재합니다.');
+    }
+
     const encryptUtil = new EncryptUtil();
     const [user_hash, user_salt] = encryptUtil.encrypt(user_pw);
 
